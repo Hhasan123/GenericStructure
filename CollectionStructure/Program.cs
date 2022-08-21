3617f465f0db992551cd3fdae974e04712a21ee9
Hhasan123 <hy907544@gmail.com>
2022-08-21 13:06:21 +0300

    Add project files.

---
+++ CollectionStructure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            Console.WriteLine(myList.Length);
+            myList.Add(54);
+            myList.Add(45);
+            Console.WriteLine(myList.Length);
+            foreach (int item in myList.Items)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
